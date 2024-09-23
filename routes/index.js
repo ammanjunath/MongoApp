3926f1ba34de7239a356b5ab944e6bc865362e37
ammanjunath <manjunathtek@gmail.com>
2024-09-23 15:17:29 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -20,54 +20,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/addTask', function(req, res, next) {
-  const taskName = req.body.taskName;
-  const createDate = Date.now();
-  
-  var task = new Task({
-    taskName: taskName,
-    createDate: createDate
-  });
-  console.log(`Adding a new task ${taskName} - createDate ${createDate}`)
-
-  task.save()
-      .then(() => { 
-        console.log(`Added new task ${taskName} - createDate ${createDate}`)        
-        res.redirect('/'); })
-      .catch((err) => {
-          console.log(err);
-          res.send('Sorry! Something went wrong.');
-      });
-});
-
-router.post('/completeTask', function(req, res, next) {
-  console.log("I am in the PUT method")
-  const taskId = req.body._id;
-  const completedDate = Date.now();
-
-  Task.findByIdAndUpdate(taskId, { completed: true, completedDate: Date.now()})
-    .then(() => { 
-      console.log(`Completed task ${taskId}`)
-      res.redirect('/'); }  )
-    .catch((err) => {
-      console.log(err);
-      res.send('Sorry! Something went wrong.');
-    });
-});
-
-
-router.post('/deleteTask', function(req, res, next) {
-  const taskId = req.body._id;
-  const completedDate = Date.now();
-  Task.findByIdAndDelete(taskId)
-    .then(() => { 
-      console.log(`Deleted task $(taskId)`)      
-      res.redirect('/'); }  )
-    .catch((err) => {
-      console.log(err);
-      res.send('Sorry! Something went wrong.');
-    });
-});
+//Code Here
 
 
 module.exports = router;
